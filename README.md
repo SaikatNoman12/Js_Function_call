a688375162f7412cc4a6553625aa6ef53db7d4f8
saikatnoman12 <noommaa240@gmail.com>
2022-09-25 13:56:37 +0600

    add js function apply()

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# JavaScript Function call() method.
+# JavaScript Function call() method and apply() method.
 
+# function call() method:-
 The javaScript call() method is a predefined JavaScript method. 
 
 It can be used to invoked (call) a method with an owner object as an argument (parameter).
@@ -8,7 +9,9 @@ call() method use another object.
 
 The call() method can accept arguments. But the first argument must always write an object. It's mandatory. Then write another arguments. Only one object are allowed. But multiple arguments can be written. So, these arguments can only be accessed within the parent object's functions.
 
-# call() method writing syntax:-
+## call() method writing syntax:-
 
 parentObjectName.functionName.call(objectName);
+
+# function apply() method:- 
  
--- call.js
+++ call.js
@@ -44,7 +44,7 @@ const total = {
 
 const total1 = {
     number1: 100,
-    number2:300
+    number2: 300
 };
 
 
@@ -54,4 +54,50 @@ const addNumber = total.addition.call(total1, 'Total number is:-', 300, 300);
 console.log(addNumber);  // return 'Total number is:- 1000'
 
 
+/* ------------ function apply() method ------------ */
+
+/* === 
+call() method and apply() method is similar type method. 
+=== */
+
+// with out function parameter:-
+const man = {
+    fullName: function () {
+        return this.firstName + ' ' + this.lastName;
+    }
+}
+
+const man2 = {
+    firstName: 'Abul',
+    lastName: 'Kalam'
+}
+
+// use apply method. Do not use any parameter;
+const manData = man.fullName.apply(man2);
+
+console.log(manData);  // return 'Abul Kalam';
+
+
+/* === 
+Use Parameter & arguments. Apply method takes argument as an array. 
+But the parameter use normally.
+=== */
+
+const women = {
+
+    // use parameter in object function.
+    fullName: function (city, country) {
+        return this.firstName + ' ' + this.lastName + ' ' + city + ' ' + country;
+    }
+}
+
+const women1 = {
+    firstName: 'Cana',
+    lastName: 'Khan'
+}
+
+// use argument in apply() method.
+const womenData =  women.fullName.apply(women1, ['Feni', 'Bangladesh']);
+
+console.log(womenData);   // return 'Sana Khan Feni Bangaldesh';
 
